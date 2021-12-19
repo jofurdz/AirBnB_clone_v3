7729c8e34d5ca4e8d4ac1109fd58db70e8a70b12
jofurdz <jofurdz@gmail.com>
2021-12-19 09:37:07 -0600

    task 4

---
+++ api/v1/views/index.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""returns status"""
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def json_status():
+    return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def status():
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
