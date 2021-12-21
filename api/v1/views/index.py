28d17862d6ce7c8c286645bddf567748900f5299
jofurdz <jofurdz@gmail.com>
2021-12-21 12:58:08 -0600

    added docstrings to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,11 +8,13 @@ from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def json_status():
+    """returns status in JSON"""
     return jsonify(status="OK")
 
 
 @app_views.route('/stats', strict_slashes=False)
 def status():
+    """counts each model"""
     return jsonify(amenities=storage.count("Amenity"),
                    cities=storage.count("City"),
                    places=storage.count("Place"),
