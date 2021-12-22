c6cae043439d868094f02fce2176f6749df556da
RockoGilbert <evolve2122@gmail.com>
2021-12-22 07:53:16 -0600

    Added a newline

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-"""app.py"""
+"""sets up Flask app"""
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from werkzeug.exceptions import HTTPException
-from flask import Flask
 from os import getenv
-from flask import jsonify
 from flask_cors import CORS
+# Start Flask app
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": '0.0.0.0'}})
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(error):
-    """teardown_appcontext"""
+def teardown(context):
+    """reloads storage after each request"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_404(error):
-    """ Returns a custom 404 error """
-    err_dict = {"error": "Not found"}
-    return jsonify(err_dict), 404
+def page_not_found(error):
+    """404 response"""
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
+    """
+    run the app host
+        defaults:
+            host='0.0.0.0'
+            port='5000'
+    """
+    host = getenv("HBNB_API_HOST", '0.0.0.0')
+    port = getenv("HBNB_API_PORT", '5000')
     app.run(host=host, port=port, threaded=True)
